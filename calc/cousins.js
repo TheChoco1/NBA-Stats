d91e1db367673f3a4964c2f7d4783d71e014f900
TheChoco1 <khaheersaza@gmail.com>
2023-02-19 19:25:20 -0500

    e

--- calc/cousins.js
+++ calc/cousins.js
@@ -9,15 +9,24 @@ import fs from 'fs';
 
 // calc from 2015-2018 (before) for demarcus cousins
 
-let bfGP = ((data2015.data[0].games_played + data2016.data[0].games_played + data2017.data[0].games_played + data2018.data[0].games_played) / 4);
-let bfPTS = ((data2015.data[0].pts + data2016.data[0].pts + data2017.data[0].pts + data2018.data[0].pts) / 4);
-let bfAST = ((data2015.data[0].ast + data2016.data[0].ast + data2017.data[0].ast + data2018.data[0].ast) / 4);
-let bfREB = ((data2015.data[0].reb + data2016.data[0].reb + data2017.data[0].reb + data2018.data[0].reb) / 4);
-let bfBLK = ((data2015.data[0].blk + data2016.data[0].blk + data2017.data[0].blk + data2018.data[0].blk) / 4);
-let bfSTL = ((data2015.data[0].stl + data2016.data[0].stl + data2017.data[0].stl + data2018.data[0].stl) / 4);
-let bfFG_PCT = ((data2015.data[0].fg_pct + data2016.data[0].fg_pct + data2017.data[0].fg_pct + data2018.data[0].fg_pct) / 4);
-let bfFG3_PCT = ((data2015.data[0].fg3_pct + data2016.data[0].fg3_pct + data2017.data[0].fg3_pct + data2018.data[0].fg3_pct) / 4);
-let bfTURNOVER = ((data2015.data[0].turnover + data2016.data[0].turnover + data2017.data[0].turnover + data2018.data[0].turnover) / 4);
+function indexOfPlayer(id) {
+    for (let i = 0; i < data2015.data.length; i++) {
+        if (data2015.data[i].player_id == id) return i;
+    }
+    return "fail"
+}
+
+let e = indexOfPlayer(107);
+
+let bfGP = ((data2015.data[e].games_played + data2016.data[e].games_played + data2017.data[e].games_played + data2018.data[e].games_played) / 4);
+let bfPTS = ((data2015.data[e].pts + data2016.data[e].pts + data2017.data[e].pts + data2018.data[e].pts) / 4);
+let bfAST = ((data2015.data[e].ast + data2016.data[e].ast + data2017.data[e].ast + data2018.data[e].ast) / 4);
+let bfREB = ((data2015.data[e].reb + data2016.data[e].reb + data2017.data[e].reb + data2018.data[e].reb) / 4);
+let bfBLK = ((data2015.data[e].blk + data2016.data[e].blk + data2017.data[e].blk + data2018.data[e].blk) / 4);
+let bfSTL = ((data2015.data[e].stl + data2016.data[e].stl + data2017.data[e].stl + data2018.data[e].stl) / 4);
+let bfFG_PCT = ((data2015.data[e].fg_pct + data2016.data[e].fg_pct + data2017.data[e].fg_pct + data2018.data[e].fg_pct) / 4);
+let bfFG3_PCT = ((data2015.data[e].fg3_pct + data2016.data[e].fg3_pct + data2017.data[e].fg3_pct + data2018.data[e].fg3_pct) / 4);
+let bfTURNOVER = ((data2015.data[e].turnover + data2016.data[e].turnover + data2017.data[e].turnover + data2018.data[e].turnover) / 4);
 
 let bf_data = {"gp" : bfGP,
                "ppg" : bfPTS,
@@ -38,15 +47,15 @@ fs.writeFile("return data/before data/cousins.json", bf_data_string, function(er
 
 // calc from 2020-2022 (after)
 
-let afGP = ((data2020.data[0].games_played + data2021.data[0].games_played) / 2);
-let afPTS = ((data2020.data[0].pts + data2021.data[0].pts) / 2);
-let afAST = ((data2020.data[0].ast + data2021.data[0].ast ) / 2);
-let afREB = ((data2020.data[0].reb + data2021.data[0].reb ) / 2);
-let afBLK = ((data2020.data[0].blk + data2021.data[0].blk ) / 2);
-let afSTL = ((data2020.data[0].stl + data2021.data[0].stl ) / 2);
-let afFG_PCT = ((data2020.data[0].fg_pct + data2021.data[0].fg_pct ) / 2);
-let afFG3_PCT = ((data2020.data[0].fg3_pct + data2021.data[0].fg3_pct) / 2);
-let afTURNOVER = ((data2020.data[0].turnover + data2021.data[0].turnover) / 2);
+let afGP = ((data2020.data[e].games_played + data2021.data[e].games_played) / 2);
+let afPTS = ((data2020.data[e].pts + data2021.data[e].pts) / 2);
+let afAST = ((data2020.data[e].ast + data2021.data[e].ast ) / 2);
+let afREB = ((data2020.data[e].reb + data2021.data[e].reb ) / 2);
+let afBLK = ((data2020.data[e].blk + data2021.data[e].blk ) / 2);
+let afSTL = ((data2020.data[e].stl + data2021.data[e].stl ) / 2);
+let afFG_PCT = ((data2020.data[e].fg_pct + data2021.data[e].fg_pct ) / 2);
+let afFG3_PCT = ((data2020.data[e].fg3_pct + data2021.data[e].fg3_pct) / 2);
+let afTURNOVER = ((data2020.data[e].turnover + data2021.data[e].turnover) / 2);
 
 let af_data = {"gp" : afGP,
                "ppg" : afPTS,
@@ -64,3 +73,5 @@ let af_data_string = JSON.stringify(af_data);
 fs.writeFile("return data/after data/cousins.json", af_data_string, function(err, result) {
     if(err) console.log('error', err);
 });
+
+console.log(e);
